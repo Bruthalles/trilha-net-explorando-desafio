c6d224a67ac65448c1f3c4e997f93c60aa479002
Bruthalles <thallesgmkr@gmail.com>
2025-06-19 17:32:12 -0300

    criando menu do hotel

--- Program.cs
+++ Program.cs
@@ -1,25 +1,49 @@
 ﻿using System.Text;
 using DesafioProjetoHospedagem.Models;
+using static System.Console;
+using System.Globalization;
 
-Console.OutputEncoding = Encoding.UTF8;
+namespace DesafioProjetoHospedage{
 
-// Cria os modelos de hóspedes e cadastra na lista de hóspedes
-List<Pessoa> hospedes = new List<Pessoa>();
+    public static class Program{
+        public static int qtdPessoas;
+        public static int diasReserva;
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+        static void Main(string[] args){
+            OutputEncoding = Encoding.UTF8;
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+            List<Pessoa> hospedes = new List<Pessoa>();
 
-// Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+            //Cadastro dinâmico
+            Write("\nQuantas pessoas quer hospedar ? ");
+            qtdPessoas = int.Parse(ReadLine());
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+            for (int i = 0; i < qtdPessoas; i++){
+                Pessoa p = new Pessoa();
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+                Write($"\nDigite o nome da {i+1}° pessoa: ");
+                p.Nome = ReadLine();
+                hospedes.Add(p);
+            }
+            // Cria a suíte
+            Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 100);
+
+            // Cria Reserva e cadastra a suite
+            Reserva reserva = new Reserva();
+            reserva.CadastrarSuite(suite);
+
+            // Verifica e cadastra os hóspedes
+            reserva.CadastrarHospedes(hospedes);
+
+            //Salvando dias reservados
+            Write("\nQuantos dias deseja se hospedar ? ");
+            diasReserva = Convert.ToInt32(ReadLine()); 
+            reserva.DiasReservados = diasReserva;
+
+            // Exibe a quantidade de hóspedes e o valor da diária
+            reserva.ObterQuantidadeHospedes();
+            reserva.CalcularValorDiaria();
+        }
+    }
+}
